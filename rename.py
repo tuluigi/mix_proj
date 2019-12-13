277454ebf76c1f892cb0f307ac4ce2464fdeee5d
luigi <>
2019-12-14 01:04:31 +0800

    add file

--- rename.py
+++ rename.py
@@ -4,12 +4,13 @@ __author__ = 'tu'
 #采用递归遍历的方式遍历图片
 import os
 #from PIL import Image
-suffer = 'xy_'
+suffer = 'mtn_'
+file_name_to_repleace='dae_noa_'
 #需要扫描的图片的目录
-assert_folder = '/Users/luigi/Documents/SYW/Test/zt-ios/ZTFutures'
+assert_folder = '/Users/luigi/Documents/SYW/MT/MTNFutures'
 
 #需要替换的文件对应的目录
-project_floder = '/Users/luigi/Documents/SYW/Test/zt-ios/ZTFutures'
+project_floder = '/Users/luigi/Documents/SYW/MT/MTNFutures'
 #不带有后缀的图片名
 file_name_list = []
 #带有后缀的文件图片
@@ -17,11 +18,17 @@ full_file_list=[]
 
 #扩展文件数组
 extension_list= [".png",".jpg",".pdf"]
+
+#修改的带有文件名dic
+#change_file_dic={}
+change_file_name_dic={}
 #黑名单
 black_name_list = ["ALIPAY_channel_choose","ALIPAY_channel_default","UNIONPAY_channel_choose","UNIONPAY_channel_default","icon_ doubt"]
 #file_name_list = ['arrow_down','default_header','icon_star_select']
 def recurve_opt(root_path):
+    #file 文件名
     for file in os.listdir(root_path):
+        #target_file完整目录路径
         target_file = os.path.join(root_path, file)
         if os.path.isfile(target_file):
             (path, extension) = os.path.splitext(target_file)
@@ -29,19 +36,36 @@ def recurve_opt(root_path):
             file_name = file.replace(extension,'')
             file_name = file_name.replace("@2x","")
             file_name = file_name.replace("@3x","")
-
+            #file_name文件名，不带有后缀
             #如果是黑名单里边的则过滤掉，直接上一个
             if file_name in black_name_list:
                 continue;
 
-            count = target_file.count(file_name)
-
-            if (extension in extension_list) and (count==2):
+            #count = target_file.count(file_name)
+            #判断是否在扩展的列表中extension_list
+            if (extension in extension_list) and (file_name_to_repleace in file_name):
+            
+                print ("file====>"+file)
                 print ("target_file====>"+target_file)
+                
+                
                 #替换后的文件名，eg:zt_xxx.jpg
-                rename_target_file = target_file.replace(file,suffer+file);
+                
+                file_rename_after = file.replace(file_name_to_repleace,suffer);
+                
+                rename_target_file = target_file.replace(file,file_rename_after);
+                print ("rename_target_file====>"+rename_target_file)
+                
                 os.rename(target_file,rename_target_file)
 
+                #替换后的name
+                file_name_after= file_name.replace(file_name_to_repleace,suffer);
+                
+                change_file_name_dic[file_name]=file_name_after
+                
+                str_shell="sed -i \'{s/\'"+file_name+"\'/\'"+file_name_after+"\'/g}\' `grep "+file_name+" -rl "+target_file+"`"
+                os.system(str_shell)
+                
                 #完整的文件名给加入到full_file_list； eg icon_home.png; 用来以后替换Assert的content.json中的
                 if file not in full_file_list:
                     full_file_list.append(file)
@@ -68,7 +92,7 @@ def rename_dir_opt(root_path):
             if file_name in file_name_list:
                 # print  "需要重名文件夹->"+target_file
                 #替换后的文件名，eg:zt_xxx.jpg
-                rename_target_file = target_file.replace(base_dir,suffer+base_dir)
+                rename_target_file = target_file.replace(file_name_to_repleace,suffer)
                 # print("rename dir =>",rename_target_file)
                 os.rename(target_file,rename_target_file);
 
@@ -78,6 +102,21 @@ def rename_dir_opt(root_path):
                 rename_dir_opt(target_file)
             else:
                 continue;
+
+def replace_file_and_name():
+    print("查询到namelist->"+','.join(file_name_list))
+    #os.system('cd '+ project_floder)
+    for file_name in change_file_name_dic:
+        origin_file_name = '\\"'+file_name+'\\"'
+        after_file_name = '\\"'+change_file_name_dic[file_name]+'\\"'
+
+        str_shell="sed -i \'{s/\'"+origin_file_name+"\'/\'"+after_file_name+"\'/g}\' `grep "+origin_file_name+" -rl "+project_floder+"`"
+
+
+        #print "shell cmd==>"+str_shell
+        print('替换代码中图片'+file_name+'--->'+change_file_name_dic[file_name])
+        os.system(str_shell)
+
 
 def replace_file_name():
     print("查询到namelist->"+','.join(file_name_list))
@@ -108,6 +147,6 @@ def replace_file_name():
 def main():
     recurve_opt(assert_folder)
     rename_dir_opt(project_floder)
-    replace_file_name()
+    replace_file_and_name()
 if __name__ == '__main__':
     main()
---
+++ rename_image.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+__author__ = 'tu'
+# 采用递归遍历的方式遍历图片
+import os
+
+# 需要扫描的图片的目录
+assert_folder = '/Users/luigi/Documents/SYW/Test/DAEInvest/Assets.xcassets'
+project_floder = '/Users/luigi/Documents/SYW/Test/DAEInvest'
+# 扩展文件数组
+extension_list = [".png", ".jpg", ".pdf"]
+# 黑名单
+black_name_list = ["ALIPAY_channel_choose", "ALIPAY_channel_default", "UNIONPAY_channel_choose",
+                   "UNIONPAY_channel_default", "icon_ doubt"]
+
+replace_imagename_dic = {}
+
+string_replace_old = 'dae_noa_'
+string_replace_new = 'mtn_'
+
+
+def recurve_opt(root_path):
+    # file 文件名
+    for file in os.listdir(root_path):
+        # target_file完整目录路径
+        target_file = os.path.join(root_path, file)
+        # print("target_file=>"+target_file)
+        # 文件路径最后的部分
+        base_dir = os.path.basename(target_file)
+
+        if os.path.isdir(target_file) and (".imageset" in base_dir):
+            pass
+            # 得到要替换的内容string
+            file_name = base_dir.replace(".imageset", "");
+            if (string_replace_old in file_name):
+                pass
+                file_name_new = file_name.replace(string_replace_old, string_replace_new);
+                target_file_new = target_file.replace(file_name, file_name_new);
+                print("file_name=>" + file_name + "; file_name_new=>" + file_name_new)
+                # 保存到数组中
+                replace_imagename_dic[target_file] = target_file_new;
+
+                os.rename(target_file,target_file_new)
+                str_shell="sed -i \'{s/\'"+file_name+"\'/\'"+file_name_new+"\'/g}\' `grep "+file_name+" -rl "+project_floder+"`"
+                os.system(str_shell)
+
+                rename_imageset(target_file_new)
+        else:
+            if os.path.isdir(target_file):
+                pass
+                recurve_opt(target_file)
+
+def rename_imageset(root_path):
+    pass
+    base_dir = os.path.basename(root_path)
+    base_dir = base_dir.replace(".imageset", "");
+    for file in os.listdir(root_path):
+        # target_file完整目录路径
+        target_file = os.path.join(root_path, file)
+        # print("target_file=>"+target_file)
+        # 文件路径最后的部分
+
+        
+        (path, extension) = os.path.splitext(target_file);
+        print("path=>"+path+";extension=>"+extension)
+        if (extension in extension_list) and (os.path.isfile(target_file)):
+            pass
+            
+            file_name = file.replace(extension,'')
+            file_name_new = base_dir
+            if ("@2x" in file_name):
+                pass
+                file_name_new=base_dir+"@2x";
+            elif ("@3x" in file_name):
+                pass
+                file_name_new=base_dir+"@3x"
+            #file_name_new=file_name_new+extension
+            #重命名文件名
+            target_file_new=os.path.join(root_path, file_name_new+extension)
+            print("---------target_file_new=>"+target_file_new)
+            os.rename(target_file,target_file_new)
+            
+            #替换imageSet
+            imgeset_content_json_path=root_path+"/Contents.json"
+            str_shell="sed -i \'{s/\'"+file_name+"\'/\'"+file_name_new+"\'/g}\' `grep "+file_name+" -rl "+imgeset_content_json_path+"`"
+            os.system(str_shell)
+
+
+
+def main():
+    recurve_opt(assert_folder)
+
+
+if __name__ == '__main__':
+    main()
