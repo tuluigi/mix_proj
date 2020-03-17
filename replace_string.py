3898a6b8932aec26e0f8cffc2804b22249846e16
luigi <>
2020-03-17 18:50:54 +0800

    修改类名命名

--- replace_string.py
+++ replace_string.py
@@ -9,7 +9,7 @@ dir_name = os.path.dirname(os.path.realpath(__file__))
 
 
 string_prefix = '  #define '
-floder_path = '/Users/luigi/Documents/ZT/NNEFutures/NNEFutures'
+floder_path = '/Users/luigi/Documents/ZT/NNEFutures/NNEFutures/'
 replaced_word_file_path = "/Users/luigi/Documents/ZT/NNEFutures/Mix_FuncationNames.h"
 dict_replace = {}
 
@@ -22,14 +22,14 @@ def read_content(filePath):
             pass
 
             line_after = line.replace(string_prefix,'')
-            print("line_after--->"+line_after)
+            #print("line_after--->"+line_after)
             list_line = line_after.split()
             #print("list_line=>"+list_line)
 
             if len(list_line)==2:
                 pass
                 dict_replace[list_line[0]]=list_line[1]
-                print("dict---key=>"+list_line[0]+";value=>"+dict_replace[list_line[0]])
+                #print("dict---key=>"+list_line[0]+";value=>"+dict_replace[list_line[0]])
     file.close()
 
     replace_string(dict_replace)
@@ -43,8 +43,9 @@ def replace_string(dict):
         string_name_new = v
 
         str_shell="sed -i \'{s/\'"+string_name_old+"\'/\'"+string_name_new+"\'/g}\' `grep "+string_name_old+" -rl "+floder_path+"`"
+        print("str_shell=>"+str_shell)
         os.system(str_shell)
-        print("replace_string_old=>"+string_name_old+";new=>"+string_name_new)
+        #print("replace_string_old=>"+string_name_old+";new=>"+string_name_new)
 
 def main():
     read_content(replaced_word_file_path)
